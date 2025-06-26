f721d5908f81d125c90336f1d82029a839a43465
RanielMiranda <189235236+astariskk@users.noreply.github.com>
2025-06-27 00:30:47 +0800

    Update index.js
    for online hosting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* remove for online hosting
+// remove for online hosting
 import express from 'express';
 const app = express();
 const port = 3000;
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Web server listening on port ${port}`);
 });
-*/
+
 
 // index.js
 import dotenv from "dotenv";
