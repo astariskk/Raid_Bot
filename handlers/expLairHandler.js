e68436f8e970ac337aa234aba59b3eaeade455d3
RanielMiranda <189235236+astariskk@users.noreply.github.com>
2025-06-27 18:40:56 +0800

    Update expLairHandler.js

--- handlers/expLairHandler.js
+++ handlers/expLairHandler.js
@@ -431,7 +431,7 @@ export function setupExpLairHandlers(client) {
         console.log(`Thread ${interaction.channel.id} now awaiting completion details.`);
 
         await interaction.editReply({
-            content: 'Please specify helpers e.g. \n`daily = @user1 @user2` \nor \n`speaker + dage = @user1 @user2` \ninclude a screenshot if possible. Or type "cancel" to close the thread.',
+            content: 'Please specify helpers e.g. \n`daily = @user1 @user2` \nor \n`speaker + dage = @user1 @user2` \nYou can use All to refer to every requested task \nInclude a screenshot if possible. \nYou can type "cancel" to close the thread.',
             ephemeral: false
         });
     });
