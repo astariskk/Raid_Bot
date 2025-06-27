dbb7b81736db0176823baf8401dd1cd170064e58
RanielMiranda <189235236+astariskk@users.noreply.github.com>
2025-06-27 23:36:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ function setupCommandsHandler(client) {
                         value: `
 \`!raidtasks\`: Lists all available raid tasks and their EXP values.
 \`!raidpoints\`: Displays the EXP values for all configured raid tasks.
+\`!checkrewards\`: Displays the daily EXP for all users.
                         `
                     },
                     {
@@ -125,7 +126,6 @@ function setupCommandsHandler(client) {
 \`!addxp @user <amount>\`: Manually adds EXP to a specified user.
 \`!removexp @user <amount>\`: Manually removes EXP from a specified user.
 \`!reset [all]\`: Resets the leaderboard (monthly automatic or force with \`all\`).
-\`!checkrewards\`: Displays the daily EXP for all users.
                         `
                     }
                 )
