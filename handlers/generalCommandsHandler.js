1a1097b73df0e26f0b12d4d1e38400e6d191d583
secret <astariskk123@gmail.com>
2025-07-01 12:15:33 +0800

    Update generalCommandsHandler.js

--- handlers/generalCommandsHandler.js
+++ handlers/generalCommandsHandler.js
@@ -145,8 +145,8 @@ export function setupGeneralCommandsHandler(client) {
                         name: '🛡️ Moderator Commands (Administrator/Officer Only)',
                         value: `
 \`!raidhelp\`: Shows a button to request raid assistance (in raid channel).
-\`!addxp @user <amount>\`: Manually adds EXP to a specified user.
-\`!removexp @user <amount>\`: Manually removes EXP from a specified user.
+\`!addxp @user @user <amount>\`: Manually adds EXP to a specified user.
+\`!removexp @user @user <amount>\`: Manually removes EXP from a specified user.
 \`!resetlb [all]\`: Resets the leaderboard (monthly automatic or force with \`all\`).
 \`!lbackup\`: Forces the bot to upload a new leaderboard backup and replace the old one.
 \`!restorelb\`: Restores the leaderboard from an attached \`leaderboard.json\` file.
