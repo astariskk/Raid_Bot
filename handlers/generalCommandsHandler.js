32095d1b3f09a230a3d6f24cae92c58dc531f9bb
secret <astariskk123@gmail.com>
2025-07-18 23:04:04 +0800

    xpcopter and tiflick

--- handlers/generalCommandsHandler.js
+++ handlers/generalCommandsHandler.js
@@ -61,6 +61,8 @@ const textGifCommands = {
     'acefault': '<@467703633618796544> [**ALWAYS AT FAURLT**](https://files.catbox.moe/chroap.gif)', // 467703633618796544
     'xyfart': '<@965985831649169438> [**BABAGAN MENYANG**](https://files.catbox.moe/kyqp98.gif)',  // 965985831649169438
     'marbike': '<@1030038861851664404> [**RIDE TO THE HARAM LAND WHERE I BELONG**](https://files.catbox.moe/ayl6ui.gif)', // 1030038861851664404
+    'tiflick': '<@385804720612048899> [**CAN YOU BLOW MY WHISTLE BABY WHISTLE BABY**](https://files.catbox.moe/14qran.gif)', // 385804720612048899
+    'xpcopter': '<@618790940290842625> [**How About This Bad Boy**](https://files.catbox.moe/k41zjv.gif)', // xp 
     'kaerat': ' https://files.catbox.moe/1leclp.gif', 
 };
 
