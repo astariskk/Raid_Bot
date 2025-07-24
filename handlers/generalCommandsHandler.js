afd52a8b8b4b0553b2f3578c94b4590309904bcb
secret <astariskk123@gmail.com>
2025-07-24 21:47:51 +0800

    added xytwerk

--- handlers/generalCommandsHandler.js
+++ handlers/generalCommandsHandler.js
@@ -75,12 +75,13 @@ export const gifCommands = {
 
 // --- Custom TEXT GIF Commands (no embeds) ---
 export const textGifCommands = {
-    'acefault': '<@467703633618796544> [**ALWAYS AT FAURLT**](https://files.catbox.moe/chroap.gif)', // 467703633618796544
-    'xyfart': '<@965985831649169438> [**BABAGAN MENYANG**](https://files.catbox.moe/kyqp98.gif)',   // 965985831649169438
-    'marbike': '<@1030038861851664404> [**RIDE TO THE HARAM LAND WHERE I BELONG**](https://files.catbox.moe/ayl6ui.gif)', // 1030038861851664404
-    'tiflick': '<@385804720612048899> [**CAN YOU BLOW MY WHISTLE BABY WHISTLE BABY**](https://files.catbox.moe/14qran.gif)', // 385804720612048899
-    'xpcopter': '<@618790940290842625> [**How About This Bad Boy?**](https://files.catbox.moe/k41zjv.gif)', // xp 
-    'kaerat': ' https://files.catbox.moe/1leclp.gif', 
+    'acefault': '<@467703633618796544> [**ALWAYS AT FAURLT**](https://files.catbox.moe/chroap.gif)', // Ace 467703633618796544
+    'xyfart': '<@965985831649169438> [**BABAGAN MENYANG**](https://files.catbox.moe/kyqp98.gif)',   // xy 965985831649169438
+    'marbike': '<@1030038861851664404> [**RIDE TO THE HARAM LAND WHERE I BELONG**](https://files.catbox.moe/ayl6ui.gif)', // Amarah 1030038861851664404
+    'tiflick': '<@385804720612048899> [**CAN YOU BLOW MY WHISTLE BABY WHISTLE BABY**](https://files.catbox.moe/14qran.gif)', // Chaos 385804720612048899
+    'xpcopter': '<@618790940290842625> [**How About This Bad Boy?**](https://files.catbox.moe/k41zjv.gif)', // xp 618790940290842625
+    'kaerat': ' https://files.catbox.moe/1leclp.gif',  // kae
+    'xytwerk': 'https://files.catbox.moe/4xeq5l.gif' // xy 
 };
 
 /**
