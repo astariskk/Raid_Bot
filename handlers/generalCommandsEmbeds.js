0c567c6bdf7fa15d55f9124201ba209fd1bd5611
secret <astariskk123@gmail.com>
2025-10-12 02:54:19 +0800

    lp to be in speaker

--- handlers/generalCommandsEmbeds.js
+++ handlers/generalCommandsEmbeds.js
@@ -224,9 +224,8 @@ export function getChartsEmbed() {
         .setTitle('📊 Available Charts')
         .setDescription(null)
         .addFields(
-            { name: 'Ultraspeaker Taunt Charts', value: '`!1man`, `!2man`, `!3man`, `!4man`' },
+            { name: 'Ultraspeaker Taunt Charts', value: '`!1man`, `!2man`, `!3man`, `!4man`, `!lpchart`',  },
             { name: 'Ultragramiel Chart', value: '`!gramielchart` or `!gramiel`' },
-            { name: 'LP Chart', value: '`!lpchart`' }
         )
         .setTimestamp()
         .setFooter({ text: 'Charts' });
